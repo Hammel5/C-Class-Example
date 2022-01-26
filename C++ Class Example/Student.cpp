b724ed0d6af7616bd17e1345ecf5c56e74656a93
Hammel5 <89536230+Hammel5@users.noreply.github.com>
2022-01-25 21:44:48 -0600

    Add project files.

---
+++ C++ Class Example/Student.cpp
@@ -0,0 +1,61 @@
+#include "Student.h"
+
+Student::Student()
+{
+	name = "";
+	id = 0;
+	classes = "";
+	grade = 0;
+	semester = "";
+	GPA = 0.0;
+}
+
+Student::~Student()
+{
+}
+
+void Student::setName(string name_)
+{
+	name = name_;
+}
+
+void Student::setID(int id_)
+{
+	id = id_;
+}
+
+void Student::setClasses(string classes_)
+{
+	classes = classes_;
+}
+
+void Student::setGrade(int grade_)
+{
+	grade = grade_;
+}
+
+void Student::setSemester(string semester_)
+{
+	semester = semester_;
+}
+
+string Student::getName()
+{
+	return name; 
+}
+
+int Student::getID()
+{
+	return id;
+}
+
+int Student::getGrade()
+{
+	return grade;
+}
+
+float Student::getGPA()
+{
+	return GPA;
+}
+
---
+++ C++ Class Example/Student.h
@@ -0,0 +1,34 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include <string>
+
+using namespace std;
+
+class Student
+{
+public:
+	Student();
+	~Student();
+
+	void setName(string name_);
+	void setID(int id_);
+	void setClasses(string classes_);
+	void setGrade(int grades_);
+	void setSemester(string semester_);
+
+	string getName();
+	int getID();
+	int getGrade();
+	float getGPA();
+
+private:
+	string name;
+	int id;
+	string classes;
+	int grade;
+	string semester;
+	float GPA;
+
+};
+#endif //!STUDENT_H
